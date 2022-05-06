ec8f4750e70b38a36990ef7b4545d88952980d8b
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:12:00 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -3,38 +3,29 @@ import config.project
 package_name = config.project.project_name
 
 console_scripts = [
-    'pymultienv=pymultienv.me:cli',
-]
-
-setup_requires = [
+    "pymultienv=pymultienv.me:cli",
 ]
 
 run_requires = [
-    'gitpython',
-    'pyfakeuse',
+    "gitpython",
+    "pyfakeuse",
 ]
 
 test_requires = [
-    'pylint',
-    'pytest',
-    'pytest-cov',
-    'pyflakes',
-    'flake8',
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "pyflakes",
+    "flake8",
 ]
 
 dev_requires = [
-    'pyclassifiers',
-    'pypitools',
-    'pydmt',
-    'Sphinx',
-    'pymakehelper',
+    "pyclassifiers",
+    "pypitools",
+    "pydmt",
+    "Sphinx",
+    "pymakehelper",
 ]
-
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {
-}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
