4eb2503e0c579916cf94b988fbd0c07af241af31
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 09:33:48 +0300

    fixed up deps

--- pymultienv/static.py
+++
@@ -1,5 +0,0 @@
-""" version which can be consumed from within the module """
-VERSION_STR = "0.0.5"
-DESCRIPTION = "pymultienv is a command to help you deal with multiple python environments"
-APP_NAME = "pymultienv"
-LOGGER_NAME = "pymultienv"
